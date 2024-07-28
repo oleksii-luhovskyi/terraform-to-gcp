f645de6cc46794cb9f924000e9158cb909fd7346
Oleksii Luhovskyi <175010602+oleksii-luhovskyi@users.noreply.github.com>
2024-07-28 16:56:12 +0300

    changed credentials

--- terraform_practice/tf_practice1/provider.tf
+++ terraform_practice/tf_practice1/provider.tf
@@ -9,12 +9,12 @@ terraform {
   backend "gcs" {
     bucket = "state-dev-storage-bucket-tf"
     prefix = "tf-practice"
-    credentials = "terraform-gcp-jenkins-project-2b58b9d2de0e.json"
+    credentials = "terraform-gcp-jenkins-project-d95c6224f967.json"
   }
 }
 
 provider "google" {
-  credentials = "terraform-gcp-jenkins-project-2b58b9d2de0e.json"
+  credentials = "terraform-gcp-jenkins-project-d95c6224f967.json"
 
   project = "terraform-gcp-jenkins-project"
   region = "us-east1"
